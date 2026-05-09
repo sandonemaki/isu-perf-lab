96fb6f0faa1f29f46fc827113efe89e343525050
makisan <48534295+sandonemaki@users.noreply.github.com>
2026-05-09 14:10:47 +0900

    makePostsNew 関数の追加です。 既存の makePosts では Go 側で del_flg の判定と 20 件の切り詰めを行っていましたが、 SQL 側で処理するようになったため、それらのロジックを除いた新しい関数を用意

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -273,6 +273,52 @@ func makePosts(ctx context.Context, results []Post, csrfToken string, allComment
 	return posts, nil
 }
 
+func makePostsNew(ctx context.Context, results []Post, csrfToken string, allComments bool) ([]Post, error) {
+	var posts []Post
+
+	for _, p := range results {
+		err := db.GetContext(ctx, &p.CommentCount, "SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?", p.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC"
+		if !allComments {
+			query += " LIMIT 3"
+		}
+		var comments []Comment
+		err = db.SelectContext(ctx, &comments, query, p.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		for i := range comments {
+			err := db.GetContext(ctx, &comments[i].User, "SELECT * FROM `users` WHERE `id` = ?", comments[i].UserID)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		// reverse
+		for i, j := 0, len(comments)-1; i < j; i, j = i+1, j-1 {
+			comments[i], comments[j] = comments[j], comments[i]
+		}
+
+		p.Comments = comments
+
+		err = db.GetContext(ctx, &p.User, "SELECT * FROM `users` WHERE `id` = ?", p.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		p.CSRFToken = csrfToken
+
+		posts = append(posts, p)
+	}
+
+	return posts, nil
+}
+
 func imageURL(p Post) string {
 	ext := ""
 	if p.Mime == "image/jpeg" {
@@ -444,13 +490,26 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.SelectContext(ctx, &results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+	query := `
+SELECT
+  posts.id as id
+  , posts.user_id as user_id
+  , posts.body as body
+  , posts.mime as mime
+  , posts.created_at as created_at
+FROM posts
+JOIN users ON posts.user_id = users.id
+WHERE users.del_flg = 0
+ORDER BY posts.created_at DESC
+LIMIT 20
+`
+	err := db.SelectContext(ctx, &results, query)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 
-	posts, err := makePosts(ctx, results, getCSRFToken(r), false)
+	posts, err := makePostsNew(ctx, results, getCSRFToken(r), false)
 	if err != nil {
 		log.Print(err)
 		return
